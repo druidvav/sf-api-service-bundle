1473f800e276568b06d83c1ba43b8aab0aff6a8e
Anton Vlasov <druidvav@gmail.com>
2026-04-22 22:18:05 +0400

    исправлено наполнение AbstractApiService

--- src/Druidvav/ApiServiceBundle/AbstractApiService.php
+++ src/Druidvav/ApiServiceBundle/AbstractApiService.php
@@ -2,6 +2,9 @@
 
 namespace Druidvav\ApiServiceBundle;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Persistence\ObjectManager;
+use LogicException;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
@@ -9,8 +12,22 @@ abstract class AbstractApiService implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
-    protected function get(string $id)
+    protected function get(string $id): ?object
     {
         return $this->container->get($id);
+    }
+
+    public function getDoctrine(): Registry
+    {
+        if (!$this->container->has('doctrine')) {
+            throw new LogicException('The DoctrineBundle is not registered in your application.');
+        }
+
+        return $this->container->get('doctrine');
+    }
+
+    protected function getEm(): ObjectManager
+    {
+        return $this->getDoctrine()->getManager();
     }
 }
